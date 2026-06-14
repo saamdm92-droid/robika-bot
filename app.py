b139ec1dd700d6cf3da696e20f2794b2ef896565
saamdm92-droid <saamdm92@gmail.com>
2026-06-15 03:23:31 +0330

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# تست مرورگر
 @app.route("/", methods=["GET"])
 def home():
     return "Bot is running!", 200
 
 
-# این همون آدرسیه که روبیکا بهش پیام می‌فرسته
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
-    print("UPDATE FROM RUKA:", data)
+    print("📩 UPDATE:", data)
     return "OK", 200
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
